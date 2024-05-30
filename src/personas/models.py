6c51a74423aa878e9068d2e47e71c5cc9626eddf
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-05-30 12:25:15 -0500

    Se modificó la clase personas añadiendo los campos de textField a nombres, apellidos y edad

--- src/personas/models.py
+++ src/personas/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class Persona(models.Model):
+    nombres   = models.TextField()
+    apellidos = models.TextField()
+    edad      = models.TextField()
 # Create your models here.
