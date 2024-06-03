5d630cd1c2b67e0a3cd2e0b0dd227ed8ec973489
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-06-03 08:37:20 -0500

    Se cambió el tipo de patron en el url para averiguar como es parturado por args y kwargs

--- src/inicio/views.py
+++ src/inicio/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def myHomeView(*args, **kwargs):
-    print("Esta es una vistaa:", args)
+    print("Esta es una vista:", args[0])
+    print("Debería de capturar el int", kwargs)
     return HttpResponse('<h1>Hola mundo</h1>')
