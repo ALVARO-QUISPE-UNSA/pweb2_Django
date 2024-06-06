39049d80871a07b082442a967fa817301d9fccb9
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-06-05 19:09:57 -0500

    Se agregó el directorio templates, se creó un file html, se añadió al ruta del template y se configuró la vista para mostrarlo

--- src/inicio/views.py
+++ src/inicio/views.py
@@ -6,7 +6,7 @@ def myHomeView(request, *args, **kwargs):
     print("args: ", args)
     print("kwargs: ", kwargs)
     print(request.user)
-    return HttpResponse('<h1>Hola Mundo desde Django</h1>')
+    return render(request, "home.html", {})
 
 def anotherView (request):
     return HttpResponse('<h1>Solo otra página</h1>')
