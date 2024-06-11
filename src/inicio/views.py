77baaa019e2fff18199284cfb84cc766e1a5395c
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-06-11 18:57:32 -0500

    Se añadió las primeras dos variables de contexto a inicio

--- src/inicio/views.py
+++ src/inicio/views.py
@@ -6,7 +6,11 @@ def myHomeView(request, *args, **kwargs):
     print("args: ", args)
     print("kwargs: ", kwargs)
     print(request.user)
-    return render(request, "home.html", {})
+    myContext = {
+        'myText': 'Esto es sobre nosotros',
+        'myNumber': 123,
+    }
+    return render(request, "home.html", myContext)
 
 def anotherView (request):
     return HttpResponse('<h1>Solo otra página</h1>')
