af5ca205f0a25d84f4a09ddf44d090ad39393551
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-06-11 21:48:49 -0500

    Se creó la vista personaCreate y el html donde se renderizará

---
+++ src/personas/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.forms import fields
+from .models import Persona
+
+class PersonaForm (forms.ModelForm):
+    class Meta:
+        model = Persona
+        fields = [
+            'nombres',
+            'apellidos',
+            'edad',
+            'donador',
+        ]
--- src/personas/views.py
+++ src/personas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Persona
+from .forms import PersonaForm
 
 def personaTestView(request):
     obj = Persona.objects.get(id=1)
@@ -7,3 +8,12 @@ def personaTestView(request):
         'objeto': obj,
     }
     return render(request, 'personas/descripcion.html', context)
+def personaCreateView(request):
+    form = PersonaForm(request.POST or None)
+    if (form.is_valid()):
+        form.save()
+        form = PersonaForm
+    context = {
+        'form': form
+    }
+    return render(request, 'personas/personasCreate.html', context)
