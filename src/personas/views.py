bace5dd2db21089fdb07b73fcc0c14c6003b6a64
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-06-14 22:45:26 -0500

    Se añadió un array para almacenar comentarios y un formulario para registarlos

--- src/personas/views.py
+++ src/personas/views.py
@@ -3,6 +3,7 @@ from .models import Persona
 from .forms import PersonaForm
 
 contador = 0
+comentarios = []
 def personaTestView(request):
     obj = Persona.objects.get(id=1)
     context = {
@@ -12,13 +13,17 @@ def personaTestView(request):
 
 def personaCreateView(request):
     global contador
+    global comentarios
     context = {
         'llenado': False,
         'contador': contador,
+        'comentarios': comentarios,
     }
     if request.method == 'POST':
         contador += 1
         nombre = request.POST.get('q')
+        com = request.POST.get('comentario')
+        comentarios.append(com)
         print(nombre)
         context['llenado'] = True
     return render(request, 'personas/personasCreate.html', context)
