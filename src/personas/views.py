3ad38afb502e81133aed98e082b444c72d7e0cc7
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-06-22 18:43:33 -0500

    Se añadió la vista del formulario

--- src/personas/views.py
+++ src/personas/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Persona
-from .forms import PersonaForm
+from .forms import PersonaForm, RawPersonaForm
 
 def personaTestView(request):
     obj = Persona.objects.get(id=1)
@@ -18,3 +18,9 @@ def personaCreateView(request):
 
 def searchForHelp(request):
     return render(request, 'personas/search.html', {})
+def personasAnotherCreateView(request):
+    form = RawPersonaForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'personas/personasCreate.html', context)
