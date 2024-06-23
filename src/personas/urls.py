dd78ab316a9ec575f509d09467860f30c1f08b17
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-06-23 16:49:24 -0500

    Se corrigió el error del \/ faltante

--- src/personas/urls.py
+++ src/personas/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
     path('<int:myID>/delete', personasDeleteView, name = 'deleting'),
     path('oldList', personasListView, name = 'listing'),
     path('', PersonaListView.as_view(), name = 'persona-list'),
-    path('<int:pk>', PersonaDetailView.as_view(), name = 'persona-detail'),
+    path('<int:pk>/', PersonaDetailView.as_view(), name = 'persona-detail'),
 ]
